d5ed9c05d38fa933366597e6cc152fe581e65060
Ahdiat Brafiadi <ahdiatgnr@gmail.com>
2024-11-26 16:03:39 +0700

    fix layout

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { GalleryVerticalEnd } from "lucide-react";
+import { GalleryVerticalEnd, Search } from "lucide-react";
+import type * as React from "react";
 
 import {
 	Sidebar,
@@ -164,10 +164,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 						<SidebarMenuButton size="lg" asChild>
 							<a href="#">
 								<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-									<GalleryVerticalEnd className="size-4" />
+									<Search className="size-4" />
 								</div>
 								<div className="flex flex-col gap-0.5 leading-none">
-									<span className="font-semibold">Documentation</span>
+									<span className="font-semibold">Cari SBM</span>
 									<span className="">v1.0.0</span>
 								</div>
 							</a>
